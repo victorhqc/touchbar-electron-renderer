e159c428d739c62248ebf516fc0844f836b924a2
Victor Quiroz Castro <victorhqc@gmail.com>
2019-06-25 16:34:53 +0200

    Add typescript prettier & linting rules

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import autoExternal from 'rollup-plugin-auto-external';
-import babel from 'rollup-plugin-babel';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
