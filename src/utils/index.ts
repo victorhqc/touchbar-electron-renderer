f436a7d46ac97369190773a14bb8fe06a0fde79a
Victor Quiroz Castro <victorhqc@gmail.com>
2019-06-26 19:30:07 +0200

    Fix lint

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,8 @@
 export * from './children';
 export * from './nativeTouchBar';
 
-export function isTruthy<Type>(x: Type | '' | null | false | undefined): x is Type {
-  return Boolean(x)
+export function isTruthy<Type>(
+  x: Type | '' | null | false | undefined,
+): x is Type {
+  return Boolean(x);
 }
