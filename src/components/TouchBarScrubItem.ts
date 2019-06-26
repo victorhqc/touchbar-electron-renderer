eecd8fade41b01071fdd09e9eada43503143933e
Victor Quiroz Castro <victorhqc@gmail.com>
2019-06-26 09:06:45 +0200

    Add types to TouchBarScrubItem

--- src/components/TouchBarScrubItem.ts
+++ src/components/TouchBarScrubItem.ts
@@ -1,39 +1,65 @@
+import { ScrubberItem, NativeImage } from 'electron';
 import uuidv4 from 'uuid/v4';
+import isEqual from 'lodash/isEqual';
 
 import TouchBarText from './TouchBarText';
-import { buildChild } from '../utils';
+import { TouchbarElement } from './types';
 
-class TouchBarScrubItem {
-  constructor(props) {
-    this.setProps(props);
+class TouchBarScrubItem implements TouchbarElement<Maybe<ScrubberItem>> {
+  id: string;
+  props: TouchBarScrubItemProps;
+  instance: Maybe<ScrubberItemIndex>;
+
+  constructor(props: TouchBarScrubItemProps) {
     this.id = uuidv4();
-
-    // TouchBarScrubItem can have only one child.
-    this.child = null;
+    this.props = props;
     this.instance = null;
   }
 
-  setProps(props) {
-    this.props = props;
+  appendChild(child: TouchBarText): void {
+    this.props.children = child;
   }
 
-  update() {}
-
-  /**
-   * TouchBarScrubItem can only have text children.
-   * @param  {stting} child
-   * @return {void}
-   */
-  appendChild(child) {
-    this.child = child;
-  }
-
-  insertBefore(child) {
+  insertBefore(child: TouchBarText): void {
     return this.appendChild(child);
   }
 
   removeChild() {
-    this.child = null;
+    this.props.children = undefined;
+  }
+
+  update({ newProps }: { newProps: TouchBarScrubItemProps }) {
+    if (isEqual(newProps, this.props)) {
+      return;
+    }
+
+    this.props = newProps;
+    this.updateInstance();
+
+    // No rerender needed when color-picker props change.
+    return false;
+  }
+
+  getNativeArgs(): ScrubberItemIndex {
+    const { children, ...props } = this.props;
+
+    return {
+      ...props,
+      label: children && children.createInstance(),
+    };
+  }
+
+  updateInstance() {
+    const args = this.getNativeArgs();
+
+    // Update instance.
+    Object.keys(args).forEach((key) => {
+      if (this.instance && this.instance[key] !== args[key]) {
+        this.instance[key] = args[key];
+      }
+    });
+
+    return this.instance;
   }
 
   createInstance() {
@@ -41,7 +67,7 @@ class TouchBarScrubItem {
 
     this.instance = {
       ...props,
-      label: buildChild(this.child),
+      label: (children && children.createInstance()) || '',
     };
 
     return this.instance;
@@ -49,3 +75,10 @@ class TouchBarScrubItem {
 }
 
 export default TouchBarScrubItem;
+
+export interface TouchBarScrubItemProps {
+  children?: TouchBarText;
+  icon?: NativeImage;
+};
+
+interface ScrubberItemIndex extends ScrubberItem, WithIndex {};
