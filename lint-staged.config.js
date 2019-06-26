97effc1d3996a38c171e9c89cd1917d814476609
Victor Quiroz Castro <victorhqc@gmail.com>
2019-06-26 11:30:23 +0200

    Fix linting warnings

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,8 @@
 module.exports = () => ({
-  '*.ts': ['prettier --config ./.prettierrc.json --write', 'eslint --fix', 'git add'],
+  '*.ts': [
+    'prettier --config ./.prettierrc.json --write',
+    'eslint --fix',
+    'git add',
+  ],
   'package.json': ['git add'],
 });
